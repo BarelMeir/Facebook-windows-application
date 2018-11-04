99eea1d5fa2bcccb9934fcf2d62671671e176260
BarelMeir <barel.meir@post.idc.ac.il>
2018-11-04 15:50:44 +0200

    initial sln

---
+++ A19 Ex01 Barel 308275445 Navot 203538608/A19_Ex01_FacebookApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A19_Ex01_FacebookApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
